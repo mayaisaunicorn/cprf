9f6cb333c42cd67059df5fc4626f60e202cb77f5
mayaisaunicorn <122163628+mayaisaunicorn@users.noreply.github.com>
2024-11-19 08:24:52 -0500

    Update abe.go

--- ro-cprf/abe.go
+++ ro-cprf/abe.go
@@ -1,34 +1,46 @@
 package abe
 
-import {
-	"rocprf"
-}
+import (
+	"math/big"
+	"github.com/ellajkim/cprf/ro-cprf/rocprf"
+	"github.com/ellajkim/cprf/ske"
+)
 
 // ABE.Setup(1λ) : It computes ppR← CPRF.Setup(1λ) and msk ← CPRF.KeyGen(pp), and outputs a public parameter pp and a master secret key msk.
-func ABESetup() {
-
-	msk, _ := KeyGen(modulus, length)
+func ABESetup(modulus *big.Int, length int) (*rocprf.MasterKey, error) {
+    msk, err := rocprf.KeyGen(modulus, length)
+    if err != nil {
+        return nil, err
+    }
+    return msk, nil
 }
 
 // ABE.KeyGen(msk, f) : It computes skf ← CPRF.Constrain(msk, f), and outputs skf .
-func ABEKeyGen(msk *MasterKey) (skf *ConstrainedKey) {
-	z := msk.z0
-	csk, _ := msk.Constrain(z)
-	skf := csk // not sure how this works, CEval has (csk *ConstrainedKey)
-	return
+func ABEKeyGen(msk *rocprf.MasterKey, f []*big.Int) (*rocprf.ConstrainedKey, error) {
+	skf, err := msk.Constrain(f)
+	if err != nil {
+		return nil, err
+	}
+	return skf, nil
 }
 
 // ABE.Enc(msk, x, msg) : It computes k ← CPRF.Eval(msk, x) and ct ← SKE.Enc(k, msg), and outputs ct
-func ABEEnc(msk *MasterKey, x []*big.Int, m string) (ct string){ // is it string?
+func ABEEnc(msk *rocprf.MasterKey, x []*big.Int, msg []byte) ([]byte, error){ // is it string?
 	k := msk.Eval(x)
-	// ct := SKEEnc(k, m)
-	// return
+	ct, err := ske.Encrypt(k, msg)
+	if err != nil {
+		return nil, err
+	}
+	return ct, nil
 }
 
 // ABE.Dec(skf , ct) : It computes k ← CPRF.CEval(skf , x), and outputs SKE.Dec(k, ct).
-func ABEDec(csk *ConstrainedKey, x []*big.Int, ct string) (fm string){
-	k = csk.CEval(x)
-	// fm := SKEDec(k, ct)
-	// return
+func ABEDec(skf *rocprf.ConstrainedKey, x []*big.Int, ct []byte) ([]byte, error){
+	k := skf.CEval(x)
+	msg, err := ske.Decrypt(k, ct)
+	if err != nil {
+		return nil, err
+	}
+	return msg, nil
 }
 
